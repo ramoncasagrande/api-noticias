c14132a15757139aade793a90fd6035b228187ff
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-07-17 17:28:25 -0300

    refatoração para user

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
 const userRoute = require("./src/routes/user.route")
 
-app.use("/home", userRoute)
+app.use(express.json());
+app.use("/user", userRoute);
 
-app.listen(3000);
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,22 @@
-const teste = (req, res) => {
-    const teste = "teste de GET";
 
-    res.send(teste);
-}
+const create = (req, res) => {
+    const { name, username, email, password, avatar, background } = req.body;
 
-module.exports = { teste }
+    if (!name || !username || !email || !password || !avatar || !background) {
+        res.status(400).send({ message: "campos não preenchidos" });
+    }
+
+
+    res.status(201).send({
+        message: "Usuario criado com sucesso",
+        user: {
+            name,
+            username,
+            email, //password não enviado na resposta
+            avatar,
+            background
+        }
+    })
+};
+
+module.exports = { create }
