8d93f45f28ae922589bb1feeb3dd5bc5ba544f1f
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-07-17 22:08:44 -0300

    CRiada função assíncrona no controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,24 @@
 
-const create = (req, res) => {
+const userService = require("../services/user.service")
+
+const create = async (req, res) => {
     const { name, username, email, password, avatar, background } = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background) {
         res.status(400).send({ message: "campos não preenchidos" });
     }
 
+    const user = await userService.create(req.body);
+
+    if (!user) {
+        return res.status(400).send({message: "Erro na criação do usuário"});
+    }
+
 
     res.status(201).send({
         message: "Usuario criado com sucesso",
         user: {
+            id: user._id,
             name,
             username,
             email, //password não enviado na resposta
