b2794683122d8ceed8e0945daf3b77fb5d295db4
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-07-25 21:18:31 -0300

    Aplicado try/catch ao projeto

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,59 +3,75 @@ const userService = require("../services/user.service");
 /* const mongoose = require("mongoose"); */
 
 const create = async (req, res) => {
-    const { name, username, email, password, avatar, background } = req.body;
+    try {
+        const { name, username, email, password, avatar, background } = req.body;
 
-    if (!name || !username || !email || !password || !avatar || !background) {
-        res.status(400).send({ message: "campos não preenchidos" });
-    }
-
-    const user = await userService.createService(req.body);
-
-    if (!user) {
-        return res.status(400).send({message: "Erro na criação do usuário"});
-    }
-
-
-    res.status(201).send({
-        message: "Usuario criado com sucesso",
-        user: {
-            id: user._id,
-            name,
-            username,
-            email, //password não enviado na resposta
-            avatar,
-            background
+        if (!name || !username || !email || !password || !avatar || !background) {
+            res.status(400).send({ message: "campos não preenchidos" });
         }
-    })
+
+        const user = await userService.createService(req.body);
+
+        if (!user) {
+            return res.status(400).send({ message: "Erro na criação do usuário" });
+        }
+
+
+        res.status(201).send({
+            message: "Usuario criado com sucesso",
+            user: {
+                id: user._id,
+                name,
+                username,
+                email, //password não enviado na resposta
+                avatar,
+                background
+            }
+        })
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
 };
 
-const findAll = async (req, res) =>{
-    const users = await userService.findAllService();
+const findAll = async (req, res) => {
+    try {
+        const users = await userService.findAllService();
 
-    if (users.length === 0){
-        return res.status(400).send({message:"Empity"})
-    }
+        if (users.length === 0) {
+            return res.status(400).send({ message: "Empity" })
+        }
 
-    res.send(users);
+        res.send(users);
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    };
 };
 
 const findById = async (req, res) => {
 
-    const user = req.user;
-    res.send(user);
+    try {
+        const user = req.user;
+        res.send(user);
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    };
 };
 
 const update = async (req, res) => {
-    const { name, username, email, password, avatar, background } = req.body;
-    const {id, user} = req;
+    try {
+        const { name, username, email, password, avatar, background } = req.body;
+        const { id, user } = req;
 
-    if (!name && !username && !email && !password && !avatar && !background) {
-        res.status(400).send({ message: "preencha pelo menos um campo" });
-    }
+        if (!name && !username && !email && !password && !avatar && !background) {
+            res.status(400).send({ message: "preencha pelo menos um campo" });
+        }
 
-    await userService.updateService(id, name, username, email, password, avatar, background );
+        await userService.updateService(id, name, username, email, password, avatar, background);
 
-    res.send({message:"Atualizado com sucesso"});
+        res.send({ message: "Atualizado com sucesso" });
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    };
 }
 
 module.exports = { create, findAll, findById, update }
--- src/middlewares/global.middleware.js
+++ src/middlewares/global.middleware.js
@@ -2,28 +2,36 @@ const mongoose = require("mongoose");
 const userService = require("../services/user.service");
 
 const validId = (req, res, next) => {
-    const id = req.params.id;
+    try {
+        const id = req.params.id;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).send({ message: "Invalid ID: " + id });
-    }
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ message: "Invalid ID: " + id });
+        }
 
-    next();
+        next();
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    };
 };
 
 const validUser = async (req, res, next) => {
-    const id = req.params.id;
+    try {
+        const id = req.params.id;
 
-    const user = await userService.findByIdService(id);
+        const user = await userService.findByIdService(id);
 
-    if (!user) {
-        return res.status(400).send({message: "User not found"});
-    }
+        if (!user) {
+            return res.status(400).send({ message: "User not found" });
+        }
 
-    req.id = id;
-    req.user = user;
+        req.id = id;
+        req.user = user;
 
-    next();
+        next();
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    };
 };
 
 module.exports = { validId, validUser }
