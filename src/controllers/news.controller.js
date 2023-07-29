c140786846966508890fcaf91b2f33da306c2076
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-07-28 23:33:43 -0300

    Validação de token

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -3,44 +3,24 @@ import { createService, findAllService } from "../services/news.service.js"
 
 const create = async (req, res) => {
     try {
-        const {authorization} = req.headers;
-        console.log(authorization);
 
-        if (!authorization) {
-            return res.send(401);
-        }
+        const { title, text, banner } = req.body;
 
-        const parts = authorization.split(" ");
-
-        const [schema, token] = parts;
-
-        if (parts !== 2) {
-            return res.send(401);
-        }
-
-        if (schema !== "Bearer") {
-            return res.send(401);
-        }
-
-        console.log(parts);
-
-        const {title, text, banner} = req.body;
-
-        if(!title || !text || !banner) {
-            res.status(400).send({message: "Submit all fields"});
+        if (!title || !text || !banner) {
+            res.status(400).send({ message: "Submit all fields" });
         }
 
         await createService({
             title,
             text,
             banner,
-            user: { _id: "64c07716d3409427fd9ae6eb" }
+            user: req.userId
         })
         res.send(201)
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
-    
+
 };
 
 const findAll = async (req, res) => {
--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { create, findAll } from "../controllers/news.controller.js"
+import { authMiddleware } from "../middlewares/auth.meddleware.js";
 
 const router = express.Router();
 
-router.post("/", create);
+router.post("/",authMiddleware, create);
 router.get("/",findAll);
 
 export default router;
