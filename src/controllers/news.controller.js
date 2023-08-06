dfb6f74f657135ce4798bc561e63a37eba457c28
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-08-06 20:40:11 -0300

    Implementado busca pelo item mais atual

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,5 +1,5 @@
 
-import { createService, findAllService, countNews } from "../services/news.service.js"
+import { createService, findAllService, countNews, topNewsService } from "../services/news.service.js"
 
 const create = async (req, res) => {
     try {
@@ -20,55 +20,85 @@ const create = async (req, res) => {
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
-
 };
 
 const findAll = async (req, res) => {
-    let { limit, offset } = req.query;
+    try {
 
-    limit = Number(limit);
-    offset = Number(offset);
+        let { limit, offset } = req.query;
 
-    if (!limit) {
-        limit = 5;
+        limit = Number(limit);
+        offset = Number(offset);
+
+        if (!limit) {
+            limit = 5;
+        }
+
+        if (!offset) {
+            offset = 0;
+        }
+
+        const news = await findAllService(offset, limit);
+        const total = await countNews();
+        const currentUrl = req.baseUrl;
+
+        const next = offset + limit;
+        const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+        const previous = offset - limit < 0 ? null : offset - limit;
+        const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+
+        if (news.length === 0) {
+            return res.status(400).send({ message: "Empity" })
+        }
+
+        res.send({
+            nextUrl,
+            previousUrl,
+            limit,
+            offset,
+            total,
+
+            results: news.map((newsItem) => ({
+                id: newsItem._id,
+                title: newsItem.title,
+                text: newsItem.text,
+                banner: newsItem.banner,
+                likes: newsItem.likes,
+                comments: newsItem.comments,
+                userName: newsItem.user.username,
+                avatar: newsItem.user.avatar
+            })),
+        });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
-
-    if (!offset) {
-        offset = 0;
-    }
-
-    const news = await findAllService(offset, limit);
-    const total = await countNews();
-    const currentUrl = req.baseUrl;
-
-    const next = offset + limit;
-    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
-
-    const previous = offset - limit < 0 ? null : offset - limit;
-    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
-
-    if (news.length === 0) {
-        return res.status(400).send({ message: "Empity" })
-    }
-
-    res.send({
-        nextUrl,
-        previousUrl,
-        limit,
-        offset,
-        total,
-
-        results: news.map((newsItem) => ({
-            id: newsItem._id,
-            title: newsItem.title,
-            text: newsItem.text,
-            banner: newsItem.banner,
-            likes: newsItem.likes,
-            comments: newsItem.comments,
-            userName: newsItem.user.username,
-            avatar: newsItem.user.avatar
-        })),
-    });
 };
 
-export { create, findAll }
+const topNews = async (req, res) => {
+
+    try {
+        const news = await topNewsService();
+
+        if (!news) {
+            return res.status(400).send({ message: "There is no post" })
+        }
+
+        res.send({
+            news: {
+                id: news._id,
+                title: news.title,
+                text: news.text,
+                banner: news.banner,
+                likes: news.likes,
+                comments: news.comments,
+                userName: news.user.username,
+                avatar: news.user.avatar
+            }
+        })
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+}
+
+export { create, findAll, topNews }
--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { create, findAll } from "../controllers/news.controller.js"
+import { create, findAll, topNews } from "../controllers/news.controller.js"
 import { authMiddleware } from "../middlewares/auth.meddleware.js";
 
 const router = express.Router();
 
 router.post("/",authMiddleware, create);
 router.get("/",findAll);
+router.get("/top", topNews)
 
 export default router;
